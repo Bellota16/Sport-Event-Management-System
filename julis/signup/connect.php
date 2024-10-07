556f7e05ae2cf741a8bfad318cff186a62835844
Bellota16 <bellotaerica7@gmail.com>
2024-10-07 22:24:34 +0800

    ERICA
    changes

--- julis/signup/connect.php
+++ julis/signup/connect.php
@@ -17,17 +17,7 @@
         if($result->num_rows > 0){
             $error = "Email address already exists. Please use a different email address.";
             echo '<script>alert("' . $error . '"); window.location.href="./signup.php";</script>';
-            exit();
-        } else {
-            // Check if password and confirm password match
-            if ($password !== $confirmpassword) {
-                $error = "Passwords do not match. Please try again.";
-                echo '<script>alert("' . $error . '"); window.location.href="./signup.php";</script>';
-                exit();
-            } else {
-                $stmt = $conn->prepare("INSERT INTO users (username, gender, email, password, confirm_password) VALUES (?, ?, ?, ?, ?)");
-                $stmt->bind_param("sssss", $username, $gender, $email, $password, $confirmpassword);
-                $execval = $stmt->execute();
+
                 if($execval){
                     echo "<script>alert('Registration successful!'); window.location.href='login.php';</script>";
                     exit();
